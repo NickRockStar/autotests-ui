18845c3450dcbcac61215d7a0df9133914a49ec0
Nick <r1.1000yzf@yandex.com>
2025-04-06 22:42:46 +0300

    Углубление в Playwright.Практика по углубленной работе с Playwright

--- playwright_registration_button.py
+++ playwright_registration_button.py
@@ -3,8 +3,11 @@ from playwright.sync_api import sync_playwright, expect
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
     page = browser.new_page()
-
+    
     page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    
+    disable_button = page.get_by_test_id('registration-page-registration-button')
+    expect(disable_button).to_be_disabled()
 
     email_input = page.locator('//*[@id=":r0:"]')
     email_input.fill('user.name@gmail.com')
@@ -12,12 +15,9 @@ with sync_playwright() as playwright:
     user_input = page.locator('//*[@id=":r1:"]')
     user_input.fill('username')
 
-    password_input = page.locator('//*[@id=":r2:"]')
+    password_input = page. locator('//*[@id=":r2:"]')
     password_input.fill('password')
 
-    registrasion_button = page.get_by_test_id('registration-page-registration-button')
-    registrasion_button.click()
+    expect(disable_button).to_be_editable()
 
-    dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashboard_title).to_be_visible()
-    expect(dashboard_title).to_have_text('Dashboard')
+    page.wait_for_timeout(3000)
