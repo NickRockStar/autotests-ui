ca34811efe27621e87a038f02ea58c17775cb71a
Nick <r1.1000yzf@yandex.com>
2025-04-06 14:34:48 +0300

    Введение в Playwright.Практика с Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -5,12 +5,17 @@ with sync_playwright() as playwright:
     page = browser.new_page()
 
     page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+
     email_input = page.locator('//*[@id=":r0:"]')
     email_input.fill('user.name@gmail.com')
+
     user_input = page.locator('//*[@id=":r1:"]')
     user_input.fill('username')
+
     password_input = page.locator('//*[@id=":r2:"]')
     password_input.fill('password')
+
     registrasion_button = page.get_by_test_id('registration-page-registration-button')
     registrasion_button.click()
+
     page.wait_for_selector('text=Dashboard', state='visible')
