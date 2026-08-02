89bec7231bdf868b58be82d1d73c7019a56af22b
NickRockStar <R1.1000YZF@yandex.com>
2026-08-02 15:54:05 +0300

    fix: delite test dashboard

--- tests/dashboard/test_dashboard.py
+++ tests/dashboard/test_dashboard.py
@@ -1,25 +1,25 @@
-import allure
-import pytest
-from allure_commons.types import Severity
-
-from config import settings
-from pages.dashboard.dashboard_page import DashboardPage
-from tools.allure.epics import AllureEpic
-from tools.allure.features import AllureFeature
-from tools.allure.stories import AllureStory
-from tools.allure.tags import AllureTag
-from tools.routes import AppRoute
-
-
-@pytest.mark.dashboard
-@pytest.mark.regression
-@allure.tag(AllureTag.REGRESSION, AllureTag.DASHBOARD)
-@allure.epic(AllureEpic.LMS)
-@allure.feature(AllureFeature.DASHBOARD)
-@allure.story(AllureStory.DASHBOARD)
-@allure.parent_suite(AllureEpic.LMS)
-@allure.suite(AllureFeature.DASHBOARD)
-@allure.sub_suite(AllureStory.DASHBOARD)
+# import allure
+# import pytest
+# from allure_commons.types import Severity
+#
+# from config import settings
+# from pages.dashboard.dashboard_page import DashboardPage
+# from tools.allure.epics import AllureEpic
+# from tools.allure.features import AllureFeature
+# from tools.allure.stories import AllureStory
+# from tools.allure.tags import AllureTag
+# from tools.routes import AppRoute
+#
+#
+# @pytest.mark.dashboard
+# @pytest.mark.regression
+# @allure.tag(AllureTag.REGRESSION, AllureTag.DASHBOARD)
+# @allure.epic(AllureEpic.LMS)
+# @allure.feature(AllureFeature.DASHBOARD)
+# @allure.story(AllureStory.DASHBOARD)
+# @allure.parent_suite(AllureEpic.LMS)
+# @allure.suite(AllureFeature.DASHBOARD)
+# @allure.sub_suite(AllureStory.DASHBOARD)
 # class TestDashboard:
 #     @allure.title("Check displaying of dashboard page")
 #     @allure.severity(Severity.NORMAL)
